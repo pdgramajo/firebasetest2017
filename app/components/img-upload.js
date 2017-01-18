0bd8f9a27c49c53895b21b6730d1df6d6b6ea83a
pdgramajo <pdp1985gramajo@gmail.com>
2017-01-18 15:49:49 +0000

    upload to firebase WORKS ok

---
+++ app/components/img-upload.js
@@ -0,0 +1,49 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+ classNameBindings: [':x-file-input', 'disabled:x-file-input--disabled'],
+  attributeBindings: ['accept'],
+  tagName: 'span',
+  tabindex: 0,
+
+  /**
+   * The text displayed when no block is passed.
+   *
+   * @property alt
+   * @default "Upload"
+   */
+  alt: "Upload",
+
+  /**
+   * Listens for change events on the native file input and dispatches
+   * the corresponding action up the context chain.
+   *
+   * @private
+   * @method
+   * @param {$.Event} e Native change event
+   */
+  change(e) {
+    this.sendAction("action", e.target.files[0], this.resetInput.bind(this));
+  },
+
+  /**
+   * Resets the value of the input so you can select the same file
+   * multiple times.
+   *
+   * @method
+   */
+  resetInput() {
+    this.$('.x-file--input').val('');
+  },
+
+  /**
+   * Generates a random ID to relate the label to the input.
+   *
+   * @method
+   * @private
+   */
+  randomId: Ember.computed(function() {
+    return  Ember.guidFor(this);
+  })
+});
+
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,9 +1,10 @@
 import Ember from 'ember';
-
 export default Ember.Controller.extend({
     needs:['application'],
+    firebaseApp: Ember.inject.service(),
     formIsVisible:false,
     application_var: Ember.computed.alias("controllers.application"),
+    imgFile:null,
     dataSelected: function () {
         var filter = this.get('filter');
         if(filter){
@@ -15,7 +16,20 @@ export default Ember.Controller.extend({
              return this.get('model');
         }
     }.property('filter'),
-    
+    //ver la url y adaptarlo
+    uploadImage(){ //https://github.com/rmmmp/emberfire-utils/blob/master/addon/services/firebase-util.js 
+       return new Ember.RSVP.Promise((resolve, reject) => {
+           const file = this.get('imgFile');
+           const metadata = {'contentType': file.type };
+           const storageRef = this.get('firebaseApp').storage().ref();
+            storageRef.child('images/' + file.name).put(file, metadata).then(function(snapshot) {
+              var url = snapshot.metadata.downloadURLs[0];
+                 resolve(url);
+             }).catch(function(error) {
+                  reject(error);
+             });
+        });
+    },
     actions:{
 		viewForm(){
 			this.set('formIsVisible',true);
@@ -23,25 +37,31 @@ export default Ember.Controller.extend({
 		hideForm(){
 		    this.set('formIsVisible',false);
 		},
+        didSelectFiles(files, resetInput) {
+             this.set('imgFile',files);
+         },
 		savePost(){
+		    this.uploadImage().then((url)=>{
+		            const title = this.get('title');
+		            const body = this.get('body');
+		             var newpost =   this.get('store').createRecord('post', {
+                          title: title,
+                          description:body
+                        });
+                        var self = this;
+                        newpost.save().then(function(post){
+                            self.transitionToRoute('post', post);
+                        }).catch(function(e){
+                            console.log('error: ',e);
+                        });
+                    
+			        this.set('formIsVisible',false);
+			        this.set('title','');
+			        this.set('body','');
+		        
+		    });
 		    
-		    const title = this.get('title');
-		    const body = this.get('body');
-		    
-		     var newpost =   this.get('store').createRecord('post', {
-                  title: title,
-                  description:body
-                });
-                var self = this;
-                newpost.save().then(function(post){
-                    self.transitionToRoute('post', post);
-                }).catch(function(e){
-                    console.log('error: ',e);
-                });
-            
-			this.set('formIsVisible',false);
-			this.set('title','');
-			this.set('body','');
+		  
 		},
 		deletePost(post){
 		/* let post1 = this.store.peekRecord('post', post.id);
